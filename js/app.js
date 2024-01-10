e8d7909e1e6f92ce4072a2a91a72203a499e8f3a
Camilo Pereira <pereiracamilo51@gmail.com>
2024-01-10 10:24:07 -0500

    Añadiendo la pagina de CarolinaSpa

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper", {
+    createElements: true,
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: true,
+    pagination: true,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    }
+});
